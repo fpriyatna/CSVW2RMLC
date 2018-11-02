c7542592992baa5f5a5f02a30909e01b87d4225d
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-11-02 12:15:25 +0100

    add csvw and logging

--- Transformer.py
+++ Transformer.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, request
+import csvw
+import logging
+
+
 app = Flask(__name__)
+logging.basicConfig(filename='csvw2rmlc.log', level=logging.DEBUG)
 
 
 @app.route('/')
@@ -11,7 +16,16 @@ def transformer():
 def result():
     if request.method == 'POST':
         result = request.form
-        return render_template("result.html", result=result)
+        csvw_url = request.form['csvw_url']
+        tg = csvw.TableGroup.from_file(csvw_url)
+        logging.warning('tg = %s', tg)
+        rmlc_url = transform_csvw_to_rmlc(csvw_url)
+        return render_template("result.html", result=result, rmlc_url=rmlc_url)
+
+
+def transform_csvw_to_rmlc(csvw_url):
+        rmlc_url = csvw_url
+        return rmlc_url
 
 
 if __name__ == '__main__':
