eced8ef88a813f0afd1378c71eb11f646ad1a077
Freddy Priyatna <freddy.priyatna@gmail.com>
2018-11-05 17:30:44 +0100

    use python native json parser

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import csvw
 import logging
-
+import json
 
 app = Flask(__name__)
 logging.basicConfig(filename='csvw2rmlc.log', level=logging.DEBUG)
@@ -21,11 +21,13 @@ def result():
 
 
 def transform_csvw_to_rmlc(csvw_url):
-        tg = csvw.TableGroup.from_file(csvw_url)
-        logging.info('tg : %s', tg)
-        logical_source = generate_logical_source(tg)
-        table_schema = tg.tableSchema
-        columns = table_schema.columns
+        #tg = csvw.TableGroup.from_file(csvw_url)
+        #logging.info('tg : %s', tg)
+        json_data = json.loads(open(csvw_url).read())
+        logging.info('json_data = \n%s', json_data)
+        logical_source = generate_logical_source(json_data)
+        table_schema = json_data['tableSchema']
+        columns = table_schema['columns']
         predicate_object_maps = generate_predicate_object_maps(columns)
         rmlc = '<TriplesMap>\n'
         rmlc = rmlc + logical_source + '\n'
@@ -35,11 +37,12 @@ def transform_csvw_to_rmlc(csvw_url):
         return rmlc
 
 
-def generate_logical_source(tg):
-    tg_url = tg.url
+def generate_logical_source(json_data):
+    url = json_data['url']
+    logging.info('url = %s', url)
     logical_source = '\trml:logicalSource[\n'
     logical_source = logical_source + '\t\trml:source'
-    logical_source = logical_source + ' "' + tg_url + '";\n'
+    logical_source = logical_source + ' "' + json_data['url'] + '";\n'
     logical_source = logical_source + '\t];\n'
     logging.info('logical_source = %s', logical_source)
     return logical_source
@@ -49,10 +52,10 @@ def generate_predicate_object_maps(columns):
     predicate_object_maps = ''
     for column in columns:
         predicate_object_map = '\trr:predicateObjectMap [\n'
-        property_url = str(column.propertyUrl)
+        property_url = str(column['propertyUrl'])
         predicate_object_map = predicate_object_map + '\t\trr:predicate ' + property_url + ';\n'
         predicate_object_map = predicate_object_map + '\t\trr:objectMap [\n'
-        column_name = column.name
+        column_name = column['name']
         predicate_object_map = predicate_object_map + '\t\t\t rr:reference "' + column_name + '";\n'
         predicate_object_map = predicate_object_map + '\t\t];\n\n'
         predicate_object_maps = predicate_object_maps + predicate_object_map
